47593e6cd0103246008114cce9f0f732e894e01f
vishal-hub-git <57080465+vishal-hub-git@users.noreply.github.com>
2021-09-24 21:57:33 +0530

    Signup Page Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header/Header";
 import HomeScreen from "./components/HomeScreen/HomeScreen";
 import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Switch>
           <Route path="/" exact component={HomeScreen}></Route>
+          <Route path="/Signup" exact component={Signup}></Route>
           <Route path="/login" component={Login}></Route>
         </Switch>
       </React.Fragment>
